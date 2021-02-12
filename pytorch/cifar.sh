1e0aef7b47fb1400ef6485a707b90579cc4c3d45
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2021-02-12 11:44:03 +0100

    Added longer pretraining

--- pytorch/cifar.sh
+++ pytorch/cifar.sh
@@ -1,9 +1,12 @@
 case $1 in
   "")
-    python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id firstModel     --phase meta_train --pre_model_id pre_firstModel
+    python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id firstModel     --phase meta_train --pre_model_id pre_firstModel --optuna_trial_nb 50
+    ;;
+  "pre_long")
+    python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id pre_firstModel_long     --phase pre_train --optuna_trial_nb 50
     ;;
   "long")
-    python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id firstModel_long     --phase meta_train --pre_model_id pre_firstModel --max_epoch 100
+    python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id firstModel_long     --phase meta_train --pre_model_id pre_firstModel --max_epoch 100 --optuna_trial_nb 50
     ;;
   "*")
     echo "no such model"

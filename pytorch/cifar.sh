dc67f511fc13a51ae9b622adea2d200099a5d379
E144069X <tristan.gomez@etu.univ-nantes.fr>
2021-02-12 09:41:49 +0100

    Added long training

--- pytorch/cifar.sh
+++ pytorch/cifar.sh
@@ -1,1 +1,11 @@
-python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id firstModel     --phase meta_train --pre_model_id pre_firstModel
+case $1 in
+  "")
+    python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id firstModel     --phase meta_train --pre_model_id pre_firstModel
+    ;;
+  "long")
+    python3 main.py --dataset_dir datasets/cifar-FS/ --exp_id cifar --model_id firstModel_long     --phase meta_train --pre_model_id pre_firstModel --max_epoch 100
+    ;;
+  "*")
+    echo "no such model"
+    ;;
+esac
